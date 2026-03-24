ecd6f3e25ebfd4e33ee82de8f8b9e468ad528ffd
Better Call Sarah <sarahalencar6409@gmail.com>
2026-03-24 10:59:14 -0300

    alterações feitas para java

---
+++ java/src/Operações.java
@@ -0,0 +1,25 @@
+public class Operações {
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    public static double diminuir(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b;
+    }
+
+    public static double expoenciar(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public static double radiciar(double a, double b) {
+        return Math.pow(a, (1/b));
+    }
+}
